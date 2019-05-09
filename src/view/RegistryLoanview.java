48c0b8823116d7bfdd9a267ab232567f86f7f144
ngo quang nam <ngonam.ptit@gmail.com>
2019-05-09 15:22:14 +0700

    fix loi

--- src/view/RegistryLoanview.java
+++ src/view/RegistryLoanview.java
@@ -549,9 +549,9 @@ public class RegistryLoanview extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        InformationLoan info = new InformationLoan();
-        info.setVisible(true);
-        this.dispose();
+//        RegisterInformation registerInformation = new RegisterInformation(user);
+//        registerInformation.setVisible(true);
+//        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void ban_dauActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ban_dauActionPerformed
